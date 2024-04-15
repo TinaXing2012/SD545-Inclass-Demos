87850c56ff98197db518bee2243e6a0ee2ef104d
Tina Xing <Tina@Tina.cs.mum.edu>
2024-04-15 09:21:07 -0500

    starter with list and add product static pages

--- W3D1-axios/client/src/App.tsx
+++ W3D1-axios/client/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import ProductList from './components/product-list';
+import AddProduct from './components/add-product';
 
 function App() {
   return (
-    <div>
-      Test
+    <div className="container">
+      <ProductList />
+      <AddProduct />
     </div>
   );
 }
