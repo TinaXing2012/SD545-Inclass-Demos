06cdd4563d80507451ba422bc29ce1842bd155fe
Tina Xing <Tina@Tina.cs.mum.edu>
2024-04-15 11:49:44 -0500

    display product list

--- W3D1-axios/client/src/components/product-list.tsx
+++ W3D1-axios/client/src/components/product-list.tsx
@@ -1,31 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import Product from '../types/product';
 
 export default function ProductList() {
+
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+        async function getProducts() {
+            const response = await axios.get('http://localhost:8000/products');
+            setProducts(response.data);
+        }
+        getProducts();
+    }, []);
+
     return (
         <div>
             <h2>Product List</h2>
             <table className="table table-striped">
                 <thead>
                     <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
+                        <th scope="col">ID</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Description</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
+                    {products.map(prod => (
+                        <tr key={prod.id}>
+                            <th scope="row">{prod.id}</th>
+                            <td>{prod.title}</td>
+                            <td>{prod.price}</td>
+                            <td>{prod.description}</td>
+                        </tr>
+                    ))}
+
                 </tbody>
             </table>
         </div>
