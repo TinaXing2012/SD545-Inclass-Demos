9e93e6c9eb3057e9994b30ee5017f316bc710ff6
Tina Xing <Tina@Tina.cs.mum.edu>
2024-04-16 11:09:44 -0500

    search feature

--- workshop4-github-search-finished/src/App.tsx
+++ workshop4-github-search-finished/src/App.tsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import List from "./components/List";
 import Search from "./components/Search";
+import User from "./types/user";
 
 export default function App() {
+    const [searchResponse, setSearchResponse] = useState<User[]>([]);
+
     return (
         <div className="container">
-            <Search />
-            <List /> 
+            <Search onSetSearchResponse={setSearchResponse} />
+            <List searchResponse={searchResponse}/> 
         </div>
     )
 }
