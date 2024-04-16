fdb2708739118c06d53744d64a9579f472ed7925
Tina Xing <Tina@Tina.cs.mum.edu>
2024-04-16 10:50:07 -0500

    static components

---
+++ workshop4-github-search-finished/src/App.tsx
@@ -0,0 +1,11 @@
+import List from "./components/List";
+import Search from "./components/Search";
+
+export default function App() {
+    return (
+        <div className="container">
+            <Search />
+            <List /> 
+        </div>
+    )
+}
