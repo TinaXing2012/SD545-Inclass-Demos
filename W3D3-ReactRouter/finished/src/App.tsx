987000e5ed5c846fcafc57a8ca3fee2a0006a138
Tina Xing <Tina@Tina.cs.mum.edu>
2024-04-17 10:42:12 -0500

    set up static page

--- W3D3-ReactRouter/finished/src/App.tsx
+++ W3D3-ReactRouter/finished/src/App.tsx
@@ -1,9 +1,34 @@
 
-
 function App() {
   return (
-    <div>
-      Hello
+    <div className="container">
+      <header className="d-flex flex-wrap justify-content-center py-3 mb-3 border-bottom">
+        <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+          <span className="fs-4">Logo</span>
+        </a>
+      </header>
+
+      <div className="row justify-content-start">
+        <div className="col-3">
+          <div className="d-flex flex-column flex-shrink-0 p-3 border">
+            <ul className="nav nav-pills flex-column mb-auto">
+              <li className="nav-item">
+                <a href="#" className="nav-link active">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="#" className="nav-link">
+                  About
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-9">
+          Home
+        </div>
+      </div>
     </div>
   );
 }
