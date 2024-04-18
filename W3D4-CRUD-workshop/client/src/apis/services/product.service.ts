76061e38c338bc73f79c348865376de4d1165c5c
Tina Xing <Tina@Tina.cs.mum.edu>
2024-04-18 14:43:14 -0500

    add feature

--- W3D4-CRUD-workshop/client/src/apis/services/product.service.ts
+++ W3D4-CRUD-workshop/client/src/apis/services/product.service.ts
@@ -1,3 +1,4 @@
+import Product from '../../types/product.type';
 import http from '../axios';
 
 const getAll = () => {
@@ -8,7 +9,12 @@ const deleteProductById = (id: number) => {
     return http.delete(`/products/${id}`);
 }
 
+const saveProd = (data: Product) => {
+    return http.post('/products', data);
+}
+
 export default {
     getAll,
-    deleteProductById
+    deleteProductById,
+    saveProd
 }
