d59195fb925ac4ea88aae5992da6892ead2a5c3e
Tina Xing <Tina@Tina.cs.mum.edu>
2024-04-22 11:12:35 -0500

    final review question 2

---
+++ final-review/src/App.tsx
@@ -0,0 +1,15 @@
+import CounterUseState from "./Question2/CounterUseState";
+import LoginUseReducer from "./Question2/LoginUseReducer";
+import LoginUseState from "./Question2/LoginUseState";
+
+function App() {
+  return (
+    <div>
+      {/* <LoginUseState /> */}
+      {/* <LoginUseReducer /> */}
+      <CounterUseState />
+    </div>
+  );
+}
+
+export default App;
